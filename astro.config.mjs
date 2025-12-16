24cd0f4e9f293090c4208d27c1736b3cf9b0a5d9
morepaperac <jasonaowu@163.com>
2025-12-17 01:43:14 +0800

    Initial commit for blog2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://morepaperac.github.io",
+	base: "/blog2025",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
